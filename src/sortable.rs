4fac3a887fe14786bac52677fbb9b669777f5480
Dani Jung <dajtg6@gmail.com>
2020-11-06 02:10:56 -0600

    Update Sortable Comment to Match Current Alias

--- src/sortable.rs
+++ src/sortable.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 
-//Creates a trait alias Sortable to combine the requirement for Partial Ordering, Partial Equality,
-//and Copyable values into a single Trait.
+//Creates a trait alias Sortable to combine the requirement for Ordered and Copyable values,
+//into a single Trait.
 //NOTE: Trait Aliasing as a proper feature is currently unstable, so this will not be required in
 //later version of Rust, and be its own official feature in the language itself.
 pub trait Sortable: Ord + Copy + fmt::Debug {}
