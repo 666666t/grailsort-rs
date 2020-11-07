75413fac5bda8f871a0265d34e6c163c041aeb56
Dani Jung <dajtg6@gmail.com>
2020-11-07 16:52:59 -0600

    Add Custom Comparator Tests

--- src/tests.rs
+++ src/tests.rs
@@ -48,6 +48,7 @@ mod tests {
             verify_sort(&set);
         }
     }
+    
     #[test]
     fn bufferless_grail_random_sizes_random_values() {
         for _ in 0..12 {
@@ -65,6 +66,22 @@ mod tests {
     }
 
     #[test]
+    fn bufferless_grail_by_value_random_sizes_random_values() {
+        for _ in 0..12 {
+            let mut rng = thread_rng();
+            let len = rng.gen_range(17, 524288);
+            let mut set: Vec<GrailPair> = (0..len)
+                .map(|x| GrailPair {
+                    key: rng.gen_range(0, len as isize),
+                    value: x as isize,
+                })
+                .collect();
+            grail_sort_by(&mut set, len, |a,b| a.value.cmp(&b.value));
+            verify_sort_values(&set);
+        }
+    }
+
+   #[test]
     fn static_buffer_grail_power_of_2_sizes() {
         for i in 8..20 {
             let mut rng = thread_rng();
@@ -121,6 +138,21 @@ mod tests {
                 .collect();
             grail_sort_with_static_buffer(&mut set, len);
             verify_sort(&set);
+        }
+    }
+    #[test]
+    fn static_buffer_grail_by_value_random_sizes_random_values() {
+        for _ in 0..12 {
+            let mut rng = thread_rng();
+            let len = rng.gen_range(17, 524288);
+            let mut set: Vec<GrailPair> = (0..len)
+                .map(|x| GrailPair {
+                    key: rng.gen_range(0, len as isize),
+                    value: x as isize,
+                })
+                .collect();
+            grail_sort_by_with_static_buffer(&mut set, len, |a,b| a.value.cmp(&b.value));
+            verify_sort_values(&set);
         }
     }
     #[test]
@@ -184,6 +216,21 @@ mod tests {
             verify_sort(&set);
         }
     }
+    #[test]
+    fn dynamic_buffer_grail_by_value_random_sizes_random_values() {
+        for _ in 0..12 {
+            let mut rng = thread_rng();
+            let len = rng.gen_range(17, 524288);
+            let mut set: Vec<GrailPair> = (0..len)
+                .map(|x| GrailPair {
+                    key: rng.gen_range(0, len as isize),
+                    value: x as isize,
+                })
+                .collect();
+            grail_sort_by_with_dynamic_buffer(&mut set, len, |a,b| a.value.cmp(&b.value));
+            verify_sort_values(&set);
+        }
+    }
 
     fn verify_sort(set: &[GrailPair]) {
         for i in 1..set.len() {
@@ -205,6 +252,18 @@ mod tests {
                     set[i].value
                 );
             }
+        }
+    }
+    fn verify_sort_values(set: &[GrailPair]) {
+        for i in 1..set.len() {
+            assert!(
+                set[i].value >= set[i - 1].value,
+                "indices {} and {} out of order; ({}, {})",
+                i - 1,
+                i,
+                set[i - 1].value,
+                set[i].value
+            );
         }
     }
 }
